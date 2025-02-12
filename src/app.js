9d9329bb36924f0ef4ea778e127cd08d7bfb5a9e
Ram Jammula <ramchowdary0308@gmail.com>
2025-02-12 13:27:04 -0500

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.use("/home",(req,res)=>{
-res.send("Sent back from server");
+app.get("/profile/:profileId",(req,res)=>{
+    console.log(req.params);
+    res.send("Namaste Ram");
+    });
+
+app.post("/profil+e",(req,res)=>{
+res.send("data posted succesfully");
 });
 
-app.use("/profile",(req,res)=>{
-    res.send("This is the profile sent from server");
+app.put("/profil(ef)",(req,res)=>{
+    res.send("data upadated succesfully");
     });
+
+app.patch("/profil*e",(req,res)=>{
+    res.send("data patches are done succesfully");
+    });
+
+app.delete("/profile",(req,res)=>{
+        res.send("data deleted succesfully");
+        });
 
 app.listen(7777,()=>{
     console.log("Request from client");
