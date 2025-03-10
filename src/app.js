b77437ec0837a0ad0c6e1e81a78376b4f7c5c25f
Ram Jammula <ramchowdary0308@gmail.com>
2025-03-10 18:49:17 -0400

    cors middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const { connectDB } = require("./config/database.js");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin:"http://localhost:5173/",
+  credentials: true,
+}));
 
 const authRouter = require("../src/routes/auth.js");
 const profileRouter = require("../src/routes/profile.js");
